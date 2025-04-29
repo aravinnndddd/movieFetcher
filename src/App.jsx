83743b4ea3cbc409f81d62f86da93828243529c1
Bhavana <aromalaravind0@gmail.com>
2025-04-29 22:38:21 +0530

    some fixes

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ const App = () => {
   const [movies, setMovies] = useState([]);
 
   useEffect(() => {
-    searchMovies("Batman");
+    searchMovies("spiderman");
   }, []);
 
   const searchMovies = async (title) => {
