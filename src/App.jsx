ee9662bf8ba698d05c1d9ae7cf70f394442b44b6
Bhavana <aromalaravind0@gmail.com>
2025-04-29 22:41:45 +0530

    some fixes

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import MovieCard from "./MovieCard";
 
 import "./App.css";
 
-const API_URL = "http://www.omdbapi.com?apikey=3c8031";
+const API_URL = "https://www.omdbapi.com?apikey=3c8031";
 
 const App = () => {
   const [searchTerm, setSearchTerm] = useState("");
